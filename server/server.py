5f9d34fe436ff5094bf2c6e6fe924bf48e243111
ThomasMyrseth <thomas.myrseth@rubberdok.no>
2024-12-20 14:31:35 +0100

    debuggin...

--- server/server.py
+++ server/server.py
@@ -981,4 +981,4 @@ def get_all_images_and_about_mes():
         return jsonify({"message": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8080)
+    app.run(debug=False, port=8080, host='0.0.0.0')
