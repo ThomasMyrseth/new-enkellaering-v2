e27959c2047da4357e34085da4406da4cf0f7aa1
ThomasMyrseth <thommyr@stud.ntnu.no>
2024-12-23 00:05:19 +0100

    added letter spacing on homepahe

--- enkellaering_ui/app/page.tsx
+++ enkellaering_ui/app/page.tsx
@@ -12,8 +12,10 @@ export default function Home() {
         <h2 className="relative z-20 md:text-4xl lg:text-7xl font-bold text-center text-black dark:text-white font-sans tracking-tight">
           <span className="text-4xl relative bg-clip-text text-transparent bg-no-repeat bg-gradient-to-r from-purple-500 via-violet-500 to-pink-500 py-4">Enkel Læring </span> <br/>{" "}
           <div className="relative mx-auto inline-block w-max [filter:drop-shadow(0px_1px_3px_rgba(27,_37,_80,_0.14))]">
-            <div className="text-xl relative bg-clip-text text-transparent bg-no-repeat bg-gradient-to-r from-purple-500 via-violet-500 to-pink-500 py-4">
-              <span className="">Slik det alltid skulle ha vært</span>
+            <div className="">
+              <p className="text-xl relative bg-clip-text text-transparent bg-no-repeat bg-gradient-to-r from-purple-500 via-violet-500 to-pink-500 py-4 leading-relaxed tracking-wide">
+                Slik det alltid skulle ha vært
+              </p>
             </div>
           </div>
         </h2>
