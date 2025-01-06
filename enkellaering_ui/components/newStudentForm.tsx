f3dcbd5170a2a3b4a34a3af1bd25f5e0b60569de
ThomasMyrseth <thommyr@stud.ntnu.no>
2025-01-07 02:06:41 +0700

    added disability to order button to prevent spam

--- enkellaering_ui/components/newStudentForm.tsx
+++ enkellaering_ui/components/newStudentForm.tsx
@@ -18,10 +18,18 @@ export default function NewStudentForm() {
     const [errorMessage, setErrorMessage] = useState<boolean | null>(null);
     const [validPhone, setValidPhone] = useState<boolean | null>(null)
     const [phone, setPhone] = useState<string>("");
+    const [isDisabled, setIsDisabled] = useState<boolean>()
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+
         e.preventDefault();
         setErrorMessage(null);
+
+        //stop user from spamming button
+        setIsDisabled(true)
+        setTimeout(() => {
+            setIsDisabled(false)
+        }, 5000); //5 seconds
 
         if (phone.length!=8) {
             setValidPhone(false)
@@ -92,9 +100,9 @@ export default function NewStudentForm() {
           />
         </LabelInputContainer>
 
-        <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[5px] dark:p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+        <button disabled={isDisabled===true} className="relative inline-flex h-12 overflow-hidden rounded-full p-[5px] dark:p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
             <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-            <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+            <span className={`${isDisabled ? "bg-slate-400" :"inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl"}`}>
                 Send inn
             </span>
         </button>
