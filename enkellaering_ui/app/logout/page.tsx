46ea20dd36c151565a7d324f4814c07564294c8a
ThomasMyrseth <thommyr@stud.ntnu.no>
2025-04-01 17:37:45 +0200

    Eslint #1

--- enkellaering_ui/app/logout/page.tsx
+++ enkellaering_ui/app/logout/page.tsx
@@ -1,9 +1,14 @@
 "use client"
 import { useRouter } from "next/navigation"
+import { useEffect } from "react"
+import React from "react"
 
 export default function LogoutPage() {
     const router = useRouter()
-    localStorage.removeItem('token')
+    useEffect(() => {
+        localStorage.removeItem('token')
+        router.push('/')
+    }, [router])
 
     router.push('/')
     return(<p>Logger ut...</p>)
