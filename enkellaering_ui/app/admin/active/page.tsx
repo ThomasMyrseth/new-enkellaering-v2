ec85f0ee145b4e0ff04feb751f8209af88d627dd
Thomas Myrseth <thommyr@stud.ntnu.no>
2026-08-18 21:37:50 +0200

    fix slow website bug

--- enkellaering_ui/app/admin/active/page.tsx
+++ enkellaering_ui/app/admin/active/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { useEffect, useState } from "react"
 import { StudentTaskWorkflow } from "../studentTasksWorkflow"
 import { TeacherTasksWorkflow } from "../teacherTasksWorkflow"
 import { StudentsWithoutAnyTeachers } from "../studentsWithoutTeacher"
@@ -7,8 +8,14 @@ import { PreviousClassesForEachTeacher } from "../previousClassesForEachTeacher"
 import { PreviousClassesForEachStudent } from "../previousClassesForEachStudent"
 
 export default function AdminActivePage() {
-    const token = localStorage.getItem('token')!
+    const [token, setToken] = useState<string | null>(null)
     const BASEURL = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:8080"
+
+    useEffect(() => {
+        setToken(localStorage.getItem('token'))
+    }, [])
+
+    if (!token) return null
 
     return (<>
         <StudentTaskWorkflow />
--- enkellaering_ui/app/admin/revenue/page.tsx
+++ enkellaering_ui/app/admin/revenue/page.tsx
@@ -1,11 +1,18 @@
 "use client"
 
+import { useEffect, useState } from "react"
 import AnalyticsOverview from "../analyticsOverview"
 import Quiz from "../quiz/main"
 import { HelpAdminPanel } from "../helpAdmin"
 
 export default function AdminRevenuePage() {
-    const token = localStorage.getItem('token')!
+    const [token, setToken] = useState<string | null>(null)
+
+    useEffect(() => {
+        setToken(localStorage.getItem('token'))
+    }, [])
+
+    if (!token) return null
 
     return (<>
         <AnalyticsOverview />
